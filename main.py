7c547f31e2292220c4dd1678cd65fdb552e7c7ac
Ben Graham <Graham.Ben61@gmail.com>
2022-02-26 16:42:08 -0700

    Captive Portal MVP

---
+++ main.py
@@ -0,0 +1,15 @@
+from wifimgr import *
+
+wlan = WifiManager().get_connection()
+
+# if security is needed:
+# wlan = WifiManager(ssid="MyAccessPoint", password="myPassword", authmode=3).get_connection()
+
+if wlan is None:
+    print("Could not initialize the network connection.")
+    while True:
+        pass  # you shall not pass :D
+
+
+# Main Code goes here, wlan is a working network.WLAN(STA_IF) instance.
+print("ESP OK")
